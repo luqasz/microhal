2b6a354baa3f7c8109710527a83925b4f318e831
Łukasz Kostka <lukasz.kostka@netng.pl>
2017-06-05 22:41:49 +0200

    Simple LED blink

--- main.c
+++ main.c
@@ -1,27 +1,13 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED_ON PORTC &= ~(1<<PC6)
-#define LED_OFF PORTC |= (1<<PC6)
-
-void sleep(uint16_t millisec)
-{
-    while(millisec)
-    {
-        _delay_ms(1);
-        millisec--;
-    }
-}
+#define LED_ON PORTC &= ~(1<<PC0)
+#define LED_OFF PORTC |= (1<<PC0)
 
 int main(void) {
-    DDRC |= (1<<PC6);
-    DDRA &= ~(1<<PA7);
-    PORTA |= (1<<PA7);
+    DDRC |= (1<<PC0);
     while (1) {
-        if ( !(PINA & (1<<PA7)) ) {
-            LED_ON;
-        } else {
-            LED_OFF;
-        }
+        DDRC ^= (1<<PC0);
+        _delay_ms(1000);
     }
 }
