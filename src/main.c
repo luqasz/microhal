9fee8dc0385cd784e9614e1da0209e9afc0636cc
Łukasz Kostka <lukasz.kostka@netng.pl>
2017-08-30 22:04:44 +0200

    Remove PCF8591 code

--- src/main.c
+++ src/main.c
@@ -4,27 +4,11 @@
 #include "uart.h"
 #include "i2c.h"
 
-#define PCF8591_ADDR 0x48
-
-void set_dac(uint8_t value) {
-    uint8_t data[] = {64, value};
-    i2c_write_buf(PCF8591_ADDR, sizeof(data), data);
-}
 
 int main(void) {
     uart_start();
-    i2c_set_speed(100);
     sei();
     uart_write("starting\r\n");
-    uint8_t i;
     while (1) {
-        for (i=0; i != 255; i++) {
-            _delay_ms(10);
-            set_dac(i);
-        }
-        for (i=255; i != 0; i--) {
-            _delay_ms(10);
-            set_dac(i);
-        }
     }
 }
