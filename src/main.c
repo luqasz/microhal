0bd87d1e2f400a551fd225575790107ff02481a4
Łukasz Kostka <lukasz.kostka@netng.pl>
2018-02-11 00:11:30 +0100

    Clean main

--- src/main.c
+++ src/main.c
@@ -1,25 +1,7 @@
 #include <avr/io.h>
-#include <stdio.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
 #include "registers.h"
-#include "uart.h"
-#include "spi.h"
-#include "hwconfig.h"
-
 
 int main(void) {
-    uart_start();
-    sei();
-    char str[32];
-    sprintf(str, "starting...\r\n");
-    uart_write(str);
-    uint16_t full_value = 0xAABB;
-    unsigned char* a = (unsigned char*)&full_value;
-    sprintf(str, "array[0] %d\r\n", a[0]);
-    uart_write(str);
-    sprintf(str, "array[1] %d\r\n", a[1]);
-    uart_write(str);
     while (1) {
     }
 }
