68cd8fbf60f8dacefc69e37c046fb1a17defe9af
Łukasz Kostka <lukasz.kostka@netng.pl>
2018-07-11 20:54:59 +0200

    Use smaller docker image

--- avr-gcc.cmake
+++ avr-gcc.cmake
@@ -1,15 +1,11 @@
 cmake_minimum_required(VERSION 3.7 FATAL_ERROR)
 set(CMAKE_CROSSCOMPILING 1)
 
-find_program(AVR_CC avr-gcc)
-find_program(AVR_CXX avr-g++)
 find_program(AVR_OBJCOPY avr-objcopy)
 find_program(AVR_SIZE_TOOL avr-size)
 find_program(AVR_OBJDUMP avr-objdump)
 find_program(AVR_UPLOADTOOL avrdude)
 
-set(CMAKE_C_COMPILER ${AVR_CC})
-set(CMAKE_CXX_COMPILER ${AVR_CXX})
 set(hex_file ${EXECUTABLE_NAME}.hex)
 set(eeprom_file ${EXECUTABLE_NAME}.eeprom)
 set(dis_file ${EXECUTABLE_NAME}.dis)
