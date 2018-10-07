48aa7d23d9a6ee89ab56f2699bb286b6b2e89fc8
Łukasz Kostka <lukasz.kostka@netng.pl>
2018-10-08 00:37:26 +0200

    Test uart with circular buffer

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <avr/io.h>
 #include "registers.h"
+#include "uart.h"
 
 int main(void) {
+    uart_start();
+    char string[] = "sine verry long string to make circular buffer full at all times";
     while (1) {
+        uart_write(string);
     }
 }
