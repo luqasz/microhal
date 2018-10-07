95dc3b997094e78b81f3fcbdfd8a762caee2c2aa
Łukasz Kostka <lukasz.kostka@netng.pl>
2018-10-07 23:24:54 +0200

    Add spi_master() comment

--- src/spi.c
+++ src/spi.c
@@ -3,11 +3,15 @@
 #include "registers.h"
 #include "spi.h"
 
+// If SS is configured as an output, the pin is a general output pin which does not affect the SPI system. Typically, the pin will be driving the SS pin of the SPI Slave.
+// If SS is configured as an input, it must be held high to ensure Master SPI operation. If the SS pin is driven low by peripheral circuitry when the SPI is configured as a Master with the SS pin defined as an input, the SPI system interprets this as another master selecting the SPI as a slave and starting to send data to it.
 void spi_master(void) {
     // Set pins as output
-    DDR_SPI |= (1<<SCK)|(1<<MOSI);
-    // Set SPI as master. Clock rate at fosc/16
-    SPCR |= (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+    DDR_SPI |= (1<<SCK);
+    DDR_SPI |= (1<<MOSI);
+
+    SPCR |= (1<<SPE);           // Enable SPI
+    SPCR |= (1<<MSTR);          // Master mode
 }
 
 void spi_send(uint8_t byte) {
