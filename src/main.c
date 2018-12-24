7f9a3f9267df9330a1c984208788bc7f5a9762ac
Łukasz Kostka <lukasz.kostka@netng.pl>
2018-12-24 22:42:31 +0100

    Test USART

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include "display/hd44780.h"
 #include "registers.h"
+#include "uart.h"
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
@@ -9,15 +9,9 @@
 int
 main(void)
 {
-    hd44780_init();
-    hd44780_set_cursor_position(0, 3);
-    hd44780_write_string("linia 1");
-    hd44780_set_cursor_position(1, 4);
-    hd44780_write_string("linia 2");
-    hd44780_set_cursor_position(2, 5);
-    hd44780_write_string("linia 3");
-    hd44780_set_cursor_position(3, 6);
-    hd44780_write_string("linia 4");
+    uart_start();
+    sei();
+    uart_write("1234567890");
     wdt_enable(WDTO_1S);
     while (1) {
         wdt_reset();
