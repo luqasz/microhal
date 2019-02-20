4ac9853d00f5b71e46d37e46fb2b7308a8fe316d
Łukasz Kostka <lukasz.kostka@netng.pl>
2019-02-20 21:10:27 +0100

    Add power of 2 circular buffer

--- src/buffer.h
+++ src/buffer.h
@@ -1,6 +1,8 @@
 #ifndef buffer_h
 #define buffer_h
 
+#include "utils.h"
+
 #include <stdint.h>
 
 namespace Buffer {
@@ -93,6 +95,42 @@ namespace Buffer {
         free() volatile const
         {
             return static_cast<uint8_t>(BUFFER_SIZE - elements);
+        }
+    };
+
+    template <uint8_t BUFFER_SIZE>
+    class CircularPowerOf2 {
+        static_assert(isPowerOfTwo(BUFFER_SIZE), "Size must be power of 2.");
+
+    private:
+        uint8_t head              = 0;
+        uint8_t tail              = 0;
+        uint8_t data[BUFFER_SIZE] = { 0 };
+        uint8_t elements          = 0;
+
+    public:
+        uint8_t
+        size() volatile
+        {
+            return static_cast<uint8_t>(head - tail);
+        }
+
+        uint8_t
+        mask(uint8_t val) volatile
+        {
+            return val % BUFFER_SIZE;
+        }
+
+        void
+        write(uint8_t byte) volatile
+        {
+            data[mask(head++)] = byte;
+        }
+
+        uint8_t
+        read() volatile
+        {
+            return data[mask(tail++)];
         }
     };
 
