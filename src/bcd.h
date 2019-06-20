0878aaf2504cb3a485efb6ed68f6226af4c86fc4
Łukasz Kostka <lukasz.kostka@netng.pl>
2019-06-20 20:35:58 +0200

    Use constexpr for BCD

--- src/bcd.h
+++ src/bcd.h
@@ -3,7 +3,16 @@
 
 #include <stdint.h>
 
-uint8_t dec_to_bcd(uint8_t);
-uint8_t bcd_to_dec(uint8_t);
+constexpr uint8_t
+dec_to_bcd(const uint8_t dec)
+{
+    return static_cast<uint8_t>(((dec / 10) << 4) | (dec % 10));
+}
+
+constexpr uint8_t
+bcd_to_dec(const uint8_t bcd)
+{
+    return static_cast<uint8_t>((((bcd & 0xF0) >> 4) * 10) + (bcd & 0x0F));
+}
 
 #endif
