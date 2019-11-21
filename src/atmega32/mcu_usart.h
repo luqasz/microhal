2b35750cea455ede65196c35656642ef1d33aeed
Łukasz Kostka <lukasz.kostka@netng.pl>
2019-11-21 19:06:57 +0100

    Format fix

--- src/atmega32/mcu_usart.h
+++ src/atmega32/mcu_usart.h
@@ -6,11 +6,11 @@
 namespace USART {
 
     struct USART0 {
-        static constexpr auto udr = Register<SFR::UDR>();
+        static constexpr auto udr   = SFR::DataRegisterRW<SFR::UDR, uint8_t>();
         static constexpr auto ucsra = Register<UCSRA<SFR::UCSRA>>();
         static constexpr auto ucsrb = Register<UCSRB<SFR::UCSRB>>();
         static constexpr auto ucsrc = Register<UCSRC<SFR::UCSRC>>();
-        static constexpr auto ubrr = set_ubrr_split<SFR::UBRRH, SFR::UBRRL, SFR::UCSRC>;
+        static constexpr auto ubrr  = set_ubrr_split<SFR::UBRRH, SFR::UBRRL, SFR::UCSRC>;
     };
 
 }
