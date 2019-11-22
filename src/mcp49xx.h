68db370e6cd76a6bd0056a937c9f837b1cdc2cf1
Łukasz Kostka <lukasz.kostka@netng.pl>
2019-11-22 22:56:13 +0100

    Remove mcp49xx.cpp

--- src/mcp49xx.h
+++ src/mcp49xx.h
@@ -1,9 +1,11 @@
-#ifndef mcp49x2_h
-#define mcp49x2_h
+#ifndef mcp49xx_h
+#define mcp49xx_h
 
 #include <stdint.h>
 
 namespace MCP49x2 {
+    constexpr uint16_t SHUTDOWN_MASK = 0xEFFF;
+    constexpr uint16_t ENABLE_BIT    = 0x1000;
 
     enum class Channel {
         A,
@@ -24,16 +26,66 @@ namespace MCP49x2 {
         uint16_t bits = 0;
 
     public:
-        void set(const MCP49x2::Gain);
-        void set(const MCP49x2::BufferControl);
-        void disable(void);
-        void enable(void);
+        void
+        set(Gain gain)
+        {
+            switch (gain) {
+                case Gain::x1:
+                    bits |= 0x2000;
+                    break;
+                case Gain::x2:
+                    bits &= 0xcfff;
+                    break;
+            }
+        }
+
+        void
+        set(BufferControl ctl)
+        {
+            switch (ctl) {
+                case BufferControl::Unbuffered:
+                    bits &= 0xbfff;
+                    break;
+                case BufferControl::Buffered:
+                    bits |= 0x4000;
+                    break;
+            }
+        }
+
+        void
+        enable()
+        {
+            bits |= ENABLE_BIT;
+        }
+
+        void
+        disable()
+        {
+            bits &= SHUTDOWN_MASK;
+        }
     };
 
-    class MCP4922 : public MCP49x2::DAC {
+    class MCP4922 : public DAC {
     public:
-        MCP4922(MCP49x2::Channel);
-        void operator=(const uint16_t);
+        MCP4922(Channel channel)
+        {
+            switch (channel) {
+                case Channel::A:
+                    bits = 0;
+                    break;
+                case Channel::B:
+                    bits = 0x8000;
+                    break;
+            }
+            enable();
+        }
+
+        void
+        operator=(uint16_t value)
+        {
+            value &= 0x0FFF;
+            bits |= value;
+        }
     };
 
 }
