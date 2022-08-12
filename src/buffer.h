4fc1bca3638c0d9280afa962f4b5e6647256af8e
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2022-08-12 23:29:28 +0200

    Use modulo in Circular buffer class

--- src/buffer.h
+++ src/buffer.h
@@ -1,13 +1,11 @@
 #pragma once
 
+#include "types.h"
 #include "utils.h"
 
-#include <stddef.h>
 #include <stdint.h>
 
 namespace Buffer {
-
-    using SIZE_TYPE = unsigned int;
 
     class Bytes {
     private:
@@ -44,15 +42,15 @@ namespace Buffer {
         }
 
         constexpr uint8_t &
-        operator[](const SIZE_TYPE idx) const
+        operator[](const usize idx) const
         {
             return begin_ptr[idx];
         };
 
-        constexpr SIZE_TYPE
+        constexpr usize
         size() const
         {
-            return static_cast<SIZE_TYPE>(end() - begin());
+            return static_cast<usize>(end() - begin());
         };
     };
 
@@ -66,68 +64,57 @@ namespace Buffer {
             Bytes(array, array + SIZE) { }
     };
 
-    template <uint8_t BUFFER_SIZE>
+    template <typename DATA_TYPE, usize BUFFER_SIZE, typename HT_TYPE = usize>
     class Circular {
-        static_assert(BUFFER_SIZE < 256, "Size bust be < 256.");
-        static_assert(BUFFER_SIZE > 0, "Size bust be > 0.");
+        static_assert(BUFFER_SIZE > 1, "Size bust be > 1.");
 
     private:
-        uint8_t head              = 0;
-        uint8_t tail              = 0;
-        uint8_t data[BUFFER_SIZE] = { 0 };
-        uint8_t elements          = 0;
+        HT_TYPE   head              = 0;
+        HT_TYPE   tail              = 0;
+        DATA_TYPE data[BUFFER_SIZE] = { 0 };
 
     public:
-        uint8_t
-        read() volatile
+        constexpr HT_TYPE
+        size() const
         {
-            uint8_t next_tail = static_cast<uint8_t>(tail + 1);
-            if (next_tail >= BUFFER_SIZE)
-                next_tail = 0;
+            return BUFFER_SIZE;
+        }
 
-            uint8_t value = data[tail];
-            tail          = next_tail;
-            elements--;
-            return value;
+        constexpr HT_TYPE
+        len() const
+        {
+            return static_cast<HT_TYPE>(head - tail);
+        }
+
+        HT_TYPE
+        mask(const HT_TYPE val)
+        {
+            return val % size();
         }
 
         void
-        write(uint8_t byte) volatile
+        write(DATA_TYPE value)
         {
-            uint8_t next_head = static_cast<uint8_t>(head + 1);
-            if (next_head >= BUFFER_SIZE)
-                next_head = 0;
-
-            data[head] = byte;
-            head       = next_head;
-            elements++;
+            data[mask(head++)] = value;
         }
 
-        // Return how many elements are in the buffer.
-        uint8_t
-        size() volatile const
+        DATA_TYPE
+        read()
         {
-            return elements;
-        }
-
-        // Return free space.
-        uint8_t
-        free() volatile const
-        {
-            return static_cast<uint8_t>(BUFFER_SIZE - elements);
+            return data[mask(tail++)];
         }
     };
 
-    template <typename DATA_TYPE, size_t BUFFER_SIZE, typename HT_TYPE = size_t>
+    template <typename DATA_TYPE, usize BUFFER_SIZE, typename HT_TYPE = usize>
     class CircularPowerOf2 {
         static_assert(isPowerOfTwo(BUFFER_SIZE), "Size must be power of 2.");
         static_assert(BUFFER_SIZE > 1, "Buffer must be at least 2 in size.");
 
     private:
-        HT_TYPE                 head              = 0;
-        HT_TYPE                 tail              = 0;
-        DATA_TYPE               data[BUFFER_SIZE] = { 0 };
-        constexpr static size_t mask              = BUFFER_SIZE - 1;
+        HT_TYPE                head              = 0;
+        HT_TYPE                tail              = 0;
+        DATA_TYPE              data[BUFFER_SIZE] = { 0 };
+        constexpr static usize mask              = BUFFER_SIZE - 1;
 
     public:
         constexpr HT_TYPE
