05e7ec78938f244ce37b043c0551d9bbfd51ec1d
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2022-08-16 19:16:37 +0200

    Typo fix

--- tests/ubrr.cpp
+++ tests/ubrr.cpp
@@ -10,65 +10,65 @@
  * Table with possible baud rates can be found in datasheet.
  */
 
-TEST_CASE("UBRR calculatio [F_CPU=1000000 BAUD=2400]")
+TEST_CASE("UBRR calculation [F_CPU=1000000 BAUD=2400]")
 {
     REQUIRE(USART::calc_ubrr(1000000_Hz, 2400, 1) == 25);
     REQUIRE(USART::calc_ubrr(1000000_Hz, 2400, 2) == 51);
 }
 
-TEST_CASE("UBRR calculatio [F_CPU=1000000 BAUD=4800]")
+TEST_CASE("UBRR calculation [F_CPU=1000000 BAUD=4800]")
 {
     REQUIRE(USART::calc_ubrr(1000000_Hz, 4800, 1) == 12);
     REQUIRE(USART::calc_ubrr(1000000_Hz, 4800, 2) == 25);
 }
 
-TEST_CASE("UBRR calculatio [F_CPU=1000000 BAUD=9600]")
+TEST_CASE("UBRR calculation [F_CPU=1000000 BAUD=9600]")
 {
     REQUIRE(USART::calc_ubrr(1000000_Hz, 9600, 1) == 6);
     REQUIRE(USART::calc_ubrr(1000000_Hz, 9600, 2) == 12);
 }
 
-TEST_CASE("UBRR calculatio [F_CPU=1000000 BAUD=14400]")
+TEST_CASE("UBRR calculation [F_CPU=1000000 BAUD=14400]")
 {
     REQUIRE(USART::calc_ubrr(1000000_Hz, 14400, 1) == 3);
     REQUIRE(USART::calc_ubrr(1000000_Hz, 14400, 2) == 8);
 }
 
-TEST_CASE("UBRR calculatio [F_CPU=1000000 BAUD=19200]")
+TEST_CASE("UBRR calculation [F_CPU=1000000 BAUD=19200]")
 {
     REQUIRE(USART::calc_ubrr(1000000_Hz, 19200, 1) == 2);
     REQUIRE(USART::calc_ubrr(1000000_Hz, 19200, 2) == 6);
 }
 
-TEST_CASE("UBRR calculatio [F_CPU=1000000 BAUD=28800]")
+TEST_CASE("UBRR calculation [F_CPU=1000000 BAUD=28800]")
 {
     REQUIRE(USART::calc_ubrr(1000000_Hz, 28800, 1) == 1);
     REQUIRE(USART::calc_ubrr(1000000_Hz, 28800, 2) == 3);
 }
 
-TEST_CASE("UBRR calculatio [F_CPU=1000000 BAUD=38400]")
+TEST_CASE("UBRR calculation [F_CPU=1000000 BAUD=38400]")
 {
     REQUIRE(USART::calc_ubrr(1000000_Hz, 38400, 1) == 1);
     REQUIRE(USART::calc_ubrr(1000000_Hz, 38400, 2) == 2);
 }
 
-TEST_CASE("UBRR calculatio [F_CPU=1000000 BAUD=57600]")
+TEST_CASE("UBRR calculation [F_CPU=1000000 BAUD=57600]")
 {
     REQUIRE(USART::calc_ubrr(1000000_Hz, 57600, 1) == 0);
     REQUIRE(USART::calc_ubrr(1000000_Hz, 57600, 2) == 1);
 }
 
-TEST_CASE("UBRR calculatio [F_CPU=1000000 BAUD=76800]")
+TEST_CASE("UBRR calculation [F_CPU=1000000 BAUD=76800]")
 {
     REQUIRE(USART::calc_ubrr(1000000_Hz, 76800, 2) == 1);
 }
 
-TEST_CASE("UBRR calculatio [F_CPU=1000000 BAUD=115200]")
+TEST_CASE("UBRR calculation [F_CPU=1000000 BAUD=115200]")
 {
     REQUIRE(USART::calc_ubrr(1000000_Hz, 115200, 2) == 0);
 }
 
-TEST_CASE("UBRR calculatio [F_CPU=11059200 BAUD=2400]")
+TEST_CASE("UBRR calculation [F_CPU=11059200 BAUD=2400]")
 {
     REQUIRE(USART::calc_ubrr(11059200_Hz, 2400, 1) == 287);
     REQUIRE(USART::calc_ubrr(11059200_Hz, 2400, 2) == 575);
