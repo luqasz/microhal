6533aa631b797b1179ad87c319eea30c01d01c40
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2022-08-16 19:13:04 +0200

    Refactor thermistor code

--- examples/thermistor.cpp
+++ examples/thermistor.cpp
@@ -1,27 +1,26 @@
 #include <thermistor.h>
 
-constexpr auto rt1 = SteinHart::ResistanceTemp {
-    25,
-    100000,
-};
-
-constexpr auto rt2 = SteinHart::ResistanceTemp {
-    50,
-    3605,
-};
-
-constexpr auto rt3 = SteinHart::ResistanceTemp {
+constexpr auto rt1 = Thermistor::CelsiusOhm {
     0,
-    32554,
+    32'554,
 };
 
-constexpr auto coef = SteinHart::calc_coef(rt1, rt2, rt3);
-
-constexpr float lookup[] = {
-    SteinHart::steinhartHart(10000, coef),
-    SteinHart::steinhartHart(20000, coef),
-    SteinHart::steinhartHart(5000, coef),
+constexpr auto rt2 = Thermistor::CelsiusOhm {
+    25,
+    10'000,
 };
+
+constexpr auto rt3 = Thermistor::CelsiusOhm {
+    50,
+    3'605,
+};
+
+constexpr auto coef         = Thermistor::calc_coef(rt1, rt2, rt3);
+constexpr auto steinhart_25 = Thermistor::calc_temp(rt2.res, coef);
+constexpr auto beta_25      = Thermistor::calc_temp(rt2.res, rt2, 3844.95f);
+
+static_assert(steinhart_25 > 24.9 && steinhart_25 < 25.1, "nominal temperature != 25C");
+static_assert(beta_25 > 24.9 && beta_25 < 25.1, "nominal temperature != 25C");
 
 int
 main(void)
--- src/thermistor.h
+++ src/thermistor.h
@@ -5,71 +5,83 @@
 
 #pragma once
 
-constexpr float KELVIN_C = 273.15;
-
-namespace SteinHart {
+/*
+Substract from kelvin to get °C
+*/
+constexpr float KELVIN_C = 273.15f;
+/*
+External links:
+    https://www.thinksrs.com/downloads/programs/therm%20calc/ntccalibrator/ntccalculator.html
+    https://www.thinksrs.com/downloads/pdfs/applicationnotes/LDC%20Note%204%20NTC%20Calculator.pdf
+*/
+namespace Thermistor {
     // Steinhart Hart coefficients.
-    struct COEF {
+    struct Coefficients {
         const float a, b, c;
     };
 
-    // Struct binding
-    // R resistance at a given temperature in celsius.
-    // T given temperature.
-    struct ResistanceTemp {
-        const u32 t;
-        const u32 r;
+    struct CelsiusOhm {
+        const float temp; // temperature in °C
+        const float res;  // resistance in Ω
     };
 
     /*
-     * Return calculated coefficients based on three values.
-     * Provide each value in steps of 25 degrees. See producers
-     * datasheet for table with defined resistances.
-     *
-     * example:
-     * 10k NTC
-     * constexpr RT {
-     *   .r = 10000,
-     *   .t = 25,
-     * };
-     * constexpr RT {
-     *   .r = 3605,
-     *   .t = 50,
-     * };
-     * constexpr RT {
-     *   .r = 1481,
-     *   .t = 75,
-     * };
-     */
-    constexpr COEF
-    calc_coef(const ResistanceTemp rt1, const ResistanceTemp rt2, const ResistanceTemp rt3)
+    Return calculated coefficients based on 3 °C,Ω pairs.
+    Provide each in steps of 25 °C.
+    See producers data sheet for those values.
+    */
+    constexpr Coefficients
+    calc_coef(const CelsiusOhm & rt1, const CelsiusOhm & rt2, const CelsiusOhm & rt3)
     {
-        const float Y1 = 1 / (static_cast<float>(rt1.t) + KELVIN_C);
-        const float Y2 = 1 / (static_cast<float>(rt2.t) + KELVIN_C);
-        const float Y3 = 1 / (static_cast<float>(rt3.t) + KELVIN_C);
+        const float Y1 = 1 / (rt1.temp + KELVIN_C);
+        const float Y2 = 1 / (rt2.temp + KELVIN_C);
+        const float Y3 = 1 / (rt3.temp + KELVIN_C);
 
-        const float L1 = log(static_cast<float>(rt1.r));
-        const float L2 = log(static_cast<float>(rt2.r));
-        const float L3 = log(static_cast<float>(rt3.r));
+        const float L1 = log(rt1.res);
+        const float L2 = log(rt2.res);
+        const float L3 = log(rt3.res);
 
         const float G2 = (Y2 - Y1) / (L2 - L1);
         const float G3 = (Y3 - Y1) / (L3 - L1);
 
-        const float c = ((G3 - G2) / (L3 - L2)) * pow(L1 + L2 + L3, -1);
-        const float b = G2 - c * (pow(L1, 2) + (L1 * L2) + pow(L2, 2));
-        const float a = Y1 - L1 * (b + c * pow(L1, 2));
-        return COEF {
+        const float c = ((G3 - G2) / (L3 - L2)) * static_cast<float>(pow(L1 + L2 + L3, -1));
+        const float b = G2 - c * (static_cast<float>(pow(L1, 2)) + (L1 * L2) + static_cast<float>(pow(L2, 2)));
+        const float a = Y1 - L1 * (b + c * static_cast<float>(pow(L1, 2)));
+        return Coefficients {
             a,
             b,
             c,
         };
     }
 
+    /*
+    Given:
+      Actual thermistor resistance in °C.
+      Thermistor coefficients
+    Return:
+      Temperature in °C
+    */
     constexpr float
-    steinhartHart(const float resistance, const COEF coef)
+    calc_temp(const float resistance, const Coefficients & coef)
     {
-        const float log_r  = log(resistance);
-        const float log_r3 = log_r * log_r * log_r;
-        return 1.0 / (coef.a + coef.b * log_r + coef.c * log_r3);
+        const float log_r     = log(resistance);
+        const float log_r3    = log_r * log_r * log_r;
+        const float in_kelvin = 1.0f / (coef.a + coef.b * log_r + coef.c * log_r3);
+        return in_kelvin - KELVIN_C;
+    }
+
+    /*
+    Given:
+      Nominal resistance and temperature
+      β value
+      Actual resistance
+    Return
+      Temperature in °C.
+    */
+    constexpr float
+    calc_temp(const float resistance, const CelsiusOhm & nominal, const float beta)
+    {
+        const float in_kelvin = 1.0f / (nominal.temp + KELVIN_C) + static_cast<float>(log(resistance / nominal.res)) / beta;
+        return (1.0f / in_kelvin) - KELVIN_C;
     }
 }
