812967781766512b8fdaf1a0d37c45860aa71706
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2022-11-05 13:38:23 +0100

    Fix BCD DEC encoding

--- tests/encoders.cpp
+++ tests/encoders.cpp
@@ -2,22 +2,22 @@
 
 #include <encoders.hpp>
 
-TEST_CASE("BCD to DEC")
-{
-    const auto bcd = BCD::from_u8(22);
-    REQUIRE(bcd.value == 34);
-}
-
 TEST_CASE("DEC to BCD")
 {
-    const auto dec = BCD::into_u8(BCD { 34 });
+    const auto bcd = encoder::dec_to_bcd(22);
+    REQUIRE(bcd == 34);
+}
+
+TEST_CASE("BCD to DEC")
+{
+    const auto dec = encoder::bcd_to_dec(34);
     REQUIRE(dec == 22);
 }
 
 TEST_CASE("DEC BCD reversable")
 {
-    const auto input_bcd = BCD { 34 };
-    const auto dec       = BCD::into_u8(input_bcd);
-    const auto bcd       = BCD::from_u8(dec);
+    const u8   input_bcd = 34;
+    const auto dec       = encoder::bcd_to_dec(input_bcd);
+    const auto bcd       = encoder::dec_to_bcd(dec);
     REQUIRE(input_bcd == bcd);
 }
