f6cb62edc3e0a20ef69100f2b6d8c748b6480d90
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2023-04-17 17:49:41 +0200

    Refactor COBS

--- include/cobs.hpp
+++ include/cobs.hpp
@@ -3,32 +3,70 @@
 #include <types.hpp>
 #include <buffer.hpp>
 
+/*
+Search through the first 254 bytes of the packet looking for the first occurrence of a zero byte.
+If no zero is found, then a code of 0xFF is output, followed by the 254 non-zero bytes.
+If a zero is found, then the number of bytes examined, n, is output as the code byte,
+followed by the non-zero bytes up to, but not including, the zero byte.
+The zero is then skipped and this process is repeated until all the bytes of the packet
+(including the final zero which is logically appended to every packet) have been encoded.
+*/
+
 namespace cobs {
+
+    constexpr const u8 *
+    cobs_end(const u8 * begin, const u8 * end)
+    {
+        const u8 * ptr = begin;
+        while (ptr != end) {
+            if (*ptr == 0 or (ptr - begin) == 254) {
+                break;
+            }
+            ptr++;
+        }
+        return ptr;
+    }
+
+    template <Writer DST>
+        requires Assignable<u8, typename DST::ContainedType>
+    constexpr usize
+    encode(const buffer::Span<const u8> in, DST & out)
+    {
+        const u8 *  current    = in.begin();
+        const usize free_start = out.free();
+        while (current < in.end()) {
+            const u8 * end = cobs_end(current, in.end());
+            const u8   len = u8(end - current);
+            out.write(len + 1);
+            while (current < end) {
+                out.write(*current++);
+            }
+            if (len < 254) {
+                current++;
+            }
+        }
+        out.write(0);
+        return free_start - out.free();
+    }
 
     constexpr usize
     encode(const buffer::Span<const u8> in, buffer::Span<u8> out)
     {
-        usize zero = 0;
-        usize dst  = 1;
-        for (const u8 & src : in) {
-            if (src == 0) {
-                out[zero] = static_cast<u8>(dst - zero);
-                zero      = dst;
-                dst += 1;
+        const u8 * current = in.begin();
+        usize      wrote   = 0;
+        while (current < in.end()) {
+            const u8 * end = cobs_end(current, in.end());
+            const u8   len = u8(end - current);
+            out[wrote++]   = len + 1;
+            while (current < end) {
+                out[wrote++] = *current++;
             }
-            else {
-                out[dst] = src;
-                dst += 1;
-                if (dst - zero == 255) {
-                    out[zero] = static_cast<u8>(dst - zero);
-                    zero      = dst;
-                    dst += 1;
-                }
+            if (len < 254) {
+                current++;
             }
         }
-        out[zero] = static_cast<u8>(dst - zero);
-        out[dst]  = 0;
-        return dst + 1;
+        out[wrote++] = 0;
+        return wrote;
     }
 
     constexpr usize
@@ -40,7 +78,7 @@ namespace cobs {
         while (in_pos < in.size()) {
             // Number of bytes to copy.
             const usize block_size = in[in_pos++] - 1;
-            for (const u8 & src : in.slice(in_pos, block_size)) {
+            for (const u8 & src : in.span(in_pos, block_size)) {
                 if (src == 0) {
                     // Encoded block can't have 0 inside.
                     return 0;
@@ -56,6 +94,14 @@ namespace cobs {
             }
         }
         return dst;
+    }
+
+    // Return required packet size based on input size.
+    constexpr usize
+    required_encoded_size(usize source_len)
+    {
+        const usize end = source_len % 254 > 0 ? 1 : 0;
+        return source_len + (source_len / 254) + end;
     }
 
 }
