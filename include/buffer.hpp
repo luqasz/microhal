0c01822610e43821507ea0bd93f9d5efa6ba122c
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2023-04-17 18:06:32 +0200

    Refactor buffers

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -2,37 +2,49 @@
 
 #include "types.hpp"
 #include "utils.hpp"
+#include <concepts.hpp>
+#include <core.hpp>
 
 namespace buffer {
 
     template <typename T>
     struct Span {
-    private:
         T *   ptr;
         usize length;
+        using ContainedType = T;
 
-        constexpr Span(T * p, const usize N) noexcept :
-            ptr(p),
-            length(N)
+        explicit constexpr Span(T * ptr_, const usize LEN) noexcept :
+            ptr(ptr_),
+            length(LEN)
         {
         }
 
-    public:
-        template <usize N>
-        constexpr Span(T (&begin_)[N]) noexcept :
-            ptr(begin_),
-            length(N)
+        template <usize LEN>
+        constexpr Span(T (&ptr_)[LEN]) noexcept :
+            ptr(ptr_),
+            length(LEN)
         {
         }
 
-        constexpr const Span<T>
-        slice(const usize b, const usize e) const
+        constexpr bool
+        contains(const ContainedType other) const
         {
-            return Span<T>(ptr + b, e);
+            for (const ContainedType & each : *this) {
+                if (each == other) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        constexpr const Span<const T>
+        span(const usize b, const usize e) const
+        {
+            return Span<const T>(ptr + b, e);
         }
 
         constexpr Span<T>
-        slice(const usize b, const usize e)
+        span(const usize b, const usize e)
         {
             return Span<T>(ptr + b, e);
         }
@@ -79,6 +91,12 @@ namespace buffer {
             return length;
         }
 
+        constexpr
+        operator Span<const T>() const
+        {
+            return Span<const T>(ptr, length);
+        }
+
         constexpr T &
         operator[](const usize idx)
         {
@@ -92,7 +110,7 @@ namespace buffer {
         }
 
         constexpr bool
-        operator==(const Span<const T> & other) const
+        operator==(const Span<T> & other) const
         {
             if (size() != other.size()) {
                 return false;
@@ -110,7 +128,33 @@ namespace buffer {
 
     template <typename Contained, usize Size>
     struct Array {
-        Contained elems[Size];
+        using ContainedType = Contained;
+        Contained data[Size];
+
+        constexpr bool
+        contains(const ContainedType other) const
+        {
+            for (const ContainedType & each : *this) {
+                if (each == other) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        constexpr usize
+        index(const ContainedType searched) const
+        {
+            usize cnt = 0;
+            for (const ContainedType & each : *this) {
+                if (each == searched) {
+                    return cnt;
+                }
+                cnt++;
+            }
+            // TODO Add Option return type
+            return 0;
+        }
 
         constexpr usize
         size() const
@@ -133,61 +177,63 @@ namespace buffer {
         constexpr Contained &
         operator[](const usize idx)
         {
-            return elems[idx];
+            return data[idx];
         }
 
         constexpr const Contained &
         operator[](const usize idx) const
         {
-            return elems[idx];
+            return data[idx];
         }
 
         constexpr Contained *
         begin()
         {
-            return elems;
+            return data;
         }
 
         constexpr const Contained *
         begin() const
         {
-            return elems;
+            return data;
         }
 
         constexpr Contained *
         end()
         {
-            return elems + Size;
+            return data + Size;
         }
 
         constexpr const Contained *
         end() const
         {
-            return elems + Size;
-        }
-
-        constexpr
-        operator Span<Contained>()
-        {
-            return Span(elems);
-        }
-
-        constexpr
-        operator const Span<const Contained>() const
-        {
-            return Span(elems);
+            return data + Size;
         }
 
         constexpr Span<Contained>
-        span(const usize sidx = 0, const usize num = Size)
+        span()
         {
-            return Span(elems).slice(sidx, num);
+            // TODO remove in favor of span() with defaults
+            return Span(data);
         }
 
         constexpr const Span<const Contained>
-        span(const usize sidx = 0, const usize num = Size) const
+        span() const
         {
-            return Span(elems).slice(sidx, num);
+            // TODO remove in favor of span() with defaults
+            return Span(data);
+        }
+
+        constexpr Span<Contained>
+        span(const usize sidx, const usize num)
+        {
+            return Span(data + sidx, num);
+        }
+
+        constexpr const Span<const Contained>
+        span(const usize sidx, const usize num) const
+        {
+            return Span(data + sidx, num);
         }
 
         constexpr bool
@@ -207,20 +253,20 @@ namespace buffer {
     template <class T, class... U>
     Array(T, U...) -> Array<T, 1 + sizeof...(U)>;
 
-    template <typename DATA_TYPE, usize BUFFER_SIZE>
+    template <typename CT, usize BUFFER_SIZE>
         requires(BUFFER_SIZE > 1)
-    class Circular {
+    struct Circular {
+        using ContainedType = CT;
+        using IndexType     = usize;
         // clangd doesn't like below assertions inside requires()
         static_assert(is_power_of_two(BUFFER_SIZE), "Buffer size must be power of 2.");
-        static_assert(BUFFER_SIZE <= ((limits<usize>::max / 2) + 1), "Buffer size must be at max half of usize.");
+        static_assert(BUFFER_SIZE <= usize((limits<IndexType>::max / 2) + 1), "Buffer size must be at max half of given index type.");
 
-    private:
-        usize                  head = 0;
-        usize                  tail = 0;
-        constexpr static usize mask = BUFFER_SIZE - 1;
-        DATA_TYPE              data[BUFFER_SIZE];
+        IndexType                  head = 0;
+        IndexType                  tail = 0;
+        constexpr static IndexType mask = BUFFER_SIZE - 1;
+        ContainedType              data[BUFFER_SIZE];
 
-    public:
         constexpr usize
         size() const
         {
@@ -240,15 +286,82 @@ namespace buffer {
         }
 
         constexpr void
-        write(const DATA_TYPE byte)
+        write(const ContainedType value)
         {
-            data[head++ & mask] = byte;
+            data[head++ & mask] = value;
         }
 
-        constexpr DATA_TYPE
+        constexpr ContainedType
         read()
         {
             return data[tail++ & mask];
         }
     };
+
+    template <typename T, typename U>
+        requires Assignable<T, U>
+    constexpr usize
+    copy(const Span<T> src, Span<U> dst)
+    {
+        usize       copied = 0;
+        const usize limit  = min(src.len(), dst.len());
+        while (copied < limit) {
+            dst[copied] = src[copied];
+            copied++;
+        }
+        return copied;
+    }
+
+    template <typename T, Writer W>
+        requires Assignable<T, typename W::ContainedType>
+    constexpr usize
+    copy(const Span<T> src, W & dst)
+    {
+        usize       copied = 0;
+        const usize limit  = min(dst.free(), src.len());
+        while (copied < limit) {
+            dst.write(src[copied]);
+            copied++;
+        }
+        return copied;
+    }
+
+    template <Reader R, typename T>
+        requires Assignable<typename R::ContainedType, T>
+    constexpr usize
+    copy(R & src, Span<T> dst)
+    {
+        usize       copied = 0;
+        const usize limit  = min(src.len(), dst.len());
+        while (copied < limit) {
+            dst[copied] = src.read();
+            copied++;
+        }
+        return copied;
+    }
+
+    template <Reader R, Writer W>
+        requires Assignable<typename R::ContainedType, typename W::ContainedType>
+    constexpr usize
+    copy(R & src, W & dst)
+    {
+        usize       copied = 0;
+        const usize limit  = min(src.len(), dst.free());
+        while (copied < limit) {
+            dst.write(src.read());
+            copied++;
+        }
+        return copied;
+    }
+
+    template <typename T, usize N, usize U>
+    constexpr Array<T, N + U>
+    operator+(const Array<T, N> & lhs, const Array<T, U> & rhs)
+    {
+        Array<T, N + U> buf { 0 };
+        copy(lhs.span(), buf.span(0, N));
+        copy(rhs.span(), buf.span(N, U));
+        return buf;
+    }
+
 }
--- include/concepts.hpp
+++ include/concepts.hpp
@@ -4,3 +4,25 @@
 
 template <typename Type>
 concept Unsigned = is_unsigned<Type>;
+
+template <class T, class U>
+concept Same = is_same<T, U>;
+
+// clang-format off
+template <typename T, typename U>
+concept Assignable = requires(T a, U b) {
+    {b = a};
+};
+
+template<class W>
+concept Writer = requires(W thing, typename W::ContainedType input) {
+    { thing.write(input) };
+    { thing.free() } -> Same<usize>;
+};
+
+template<class R>
+concept Reader = requires(R thing, typename R::ContainedType input) {
+    { thing.read() } -> Same<typename R::ContainedType>;
+    { thing.len() } -> Same<usize>;
+};
+// clang-format on
