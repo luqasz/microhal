80664e79acd775f4cb5ecc930d356d47124fbdcf
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2023-10-13 03:10:54 +0200

    Fix rtc example

--- examples/rtc.cpp
+++ examples/rtc.cpp
@@ -14,15 +14,15 @@ constexpr units::Frequency fcpu = units::Hz * 11059200;
 
 constexpr auto config = USART::Config {
     .char_size = USART::CharacterSize::Bit8,
-    .parity    = USART::Parity::None,
+    .parity = USART::Parity::None,
     .stop_bits = USART::StopBits::One,
-    .ubrr      = USART::ubrr<fcpu, 115200, 2>(),
+    .ubrr = USART::ubrr<fcpu, 115200, 2>(),
 };
 
 constexpr auto rtc_target = i2c::Target {
-    .address       = ds1337::I2C_ADDRESS,
+    .address = ds1337::I2C_ADDRESS,
     .start_address = u8(ds1337::Register::Seconds),
-    .speed         = units::kHz * 400,
+    .speed = units::kHz * 400,
 };
 
 using USART_0 = USART::Async<USART::usart0>;
@@ -32,25 +32,25 @@ main()
 {
     ds1337::DateTime_Array rtc_buffer { 0 };
 
-    auto usart  = USART_0().set(config).enable(USART::Channel::TX);
+    auto usart = USART_0().set(config).enable(USART::Channel::TX);
     auto serial = Printer(usart, LineEnd::CRLF);
-    auto bus    = i2c::Master<i2c::i2c0>(fcpu);
-    auto dt     = DateTime {
-            .second = 50,
-            .minute = 59,
-            .hour   = 23,
-            .day    = 30,
-            .month  = 11,
-            .year   = 2019,
+    auto bus = i2c::Master<i2c::i2c0>(fcpu);
+    auto dt = DateTime {
+        .second = 50,
+        .minute = 59,
+        .hour = 23,
+        .day = 30,
+        .month = 11,
+        .year = 2019,
     };
 
     serial.printLn("Setting date and time.");
-    ds1337::set(dt, rtc_buffer);
-    bus.write(rtc_buffer, rtc_target);
+    ds1337::set(dt, rtc_buffer.span());
+    bus.write(rtc_buffer.span(), rtc_target);
     char strbuf[30];
     while (true) {
-        bus.read(rtc_buffer, rtc_target);
-        ds1337::get(dt, rtc_buffer);
+        bus.read(rtc_buffer.span(), rtc_target);
+        ds1337::get(dt, rtc_buffer.span());
         snprintf(strbuf, 30, "%d.%d.%d %d:%d", dt.year, dt.month, dt.hour, dt.minute, dt.second);
         serial.printLn(strbuf);
         _delay_ms(1000);
